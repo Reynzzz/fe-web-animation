48b439c8ad1ab4bb373b2eb9bf1c32d3b8d9adbf
Reynzzz <Rayhanay115@gmail.com>
2026-06-21 22:22:34 +0700

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
@@ -13,6 +13,7 @@ export default defineConfig(({mode}) => {
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
+        'hls.js': 'hls.js/dist/hls.js', // <--- INI JALAN PINTASNYA
       },
     },
     server: {
